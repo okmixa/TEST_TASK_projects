184649ad3adddf7801095e679a98bd80e7acb4dc
Mykhailo Rylov <mykhailo.rylov@envisiontec.de>
2022-02-06 21:31:51 +0200

    add projects : 1) Qt_FileTransfer, 2) Qt_botmap_upd_sender

--- Qt_Network_Map/QuickContainers/src/qcmPlugin.cpp
+++
@@ -1,9 +0,0 @@
-#include "./qcmPlugin.h"
-#include "qcmAbstractContainer.h"
-#include "qcmContainer.h"
-
-void QuickContainersPlugin::registerTypes(const char *uri)
-{
-    // @uri QuickContainers
-    qmlRegisterType< qcm::AbstractContainer >(uri, 1, 0, "AbstractContainer" );
-}
--- Qt_Network_Map/doc/reference/main.md
+++
@@ -1,9 +0,0 @@
-QuickQanava  {#mainpage}
-===========
-
-- See QuickQanava GitHub: https://github.com/cneben/quickqanava
-
-- Or homepage: http://www.destrat.io/quickqanava
-
-
-
